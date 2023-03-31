c4eff825fd6394921a1c21517ee85d3c04d0e3a7
Alessandro Molina <amol@turbogears.org>
2023-03-31 13:18:16 +0200

    load data from PQT

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # dumdb
 Experimental Dumb Database written in Ibis+Arrow+Substrait
+
+## Try
+Run:
+```
+$ python experiment.py "SELECT first_name,last_name,title FROM userdata1 WHERE country='Canada'"
+```
--- experiment.py
+++ experiment.py
@@ -1,19 +1,51 @@
+import glob
+import os
+import argparse
 import pyarrow
-t = pyarrow.Table.from_pydict({"fruit": ["apple", "apple", "banana", "orange"], "price": [0.5, 0.5, 0.25, 0.33]})
-
+import pyarrow.parquet
+import pyarrow.substrait
 import ibis
 import ibis.expr.schema as sch
 import ibis.backends.pyarrow.datatypes
-table_schema = sch.schema(t.schema)
-
 import ibis.expr.sql
-x = ibis.expr.sql.parse_sql("SELECT price FROM fruits WHERE fruit='banana'", catalog={"fruits": table_schema})
-
 from ibis_substrait.compiler.core import SubstraitCompiler
-compiler = SubstraitCompiler()
-plan = compiler.compile(x)
 
-import pyarrow.substrait
-r = pyarrow.substrait.run_query(pyarrow.py_buffer(plan.SerializeToString()), table_provider=lambda *args: t)
+def main():
+    parser = argparse.ArgumentParser(
+        description = 'Run SQL queries on parquet files'
+    )
+    parser.add_argument(
+        "query", 
+        help="The query to run"
+    )
+    opts = parser.parse_args()
+    
+    # Detect all available Tables and their schemas
+    catalog = {}
+    for pqtfile in glob.glob("*.parquet"):
+        table_name, _ = os.path.splitext(os.path.basename(pqtfile))
+        table_schema = pyarrow.parquet.read_schema(pqtfile)
+        catalog[table_name] = sch.schema(table_schema)
+    print("CATALOG", list(catalog.keys()))
 
-print(r.read_all())
+    # Parse the query to Substrait
+    q = ibis.expr.sql.parse_sql(opts.query, catalog=catalog)
+    compiler = SubstraitCompiler()
+    plan = compiler.compile(q)
+
+    # Run the query
+    result = pyarrow.substrait.run_query(pyarrow.py_buffer(plan.SerializeToString()), 
+                                         table_provider=table_provider)
+    result_table = result.read_all()
+    print(result_table.to_pandas())
+
+
+def table_provider(table_name):
+    filename = f"{table_name[0]}.parquet"
+    print("LOADING TABLE", filename)
+    table = pyarrow.parquet.read_table(filename)
+    return table
+
+
+if __name__ == "__main__":
+    main()
